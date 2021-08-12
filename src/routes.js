f2b0fa948b7a004410b04fa2fefd4bfce2b7e017
Alexander Adam <alexanderadamm21@gmail.com>
2021-08-13 01:33:24 +0700

    restructure with hapi plugin

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,34 @@
-const {
-    addNoteHandler, 
-    getAllNotesHandler, 
-    getNoteByIdHandler, 
-    editNoteHandler,
-    deleteNoteHandler, 
-} = require('./handler');
+const { deleteNoteByIdHandler } = require('./handler');
+const { editNoteByIdHandler } = require('./handler');
+const { getNoteByIdHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: getAllNotesHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: getNoteByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: deleteNoteHandler,
-    },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
 ];
 
 module.exports = routes;
